ed0caf5bfc06b84affcde18c4001cf887895fdee
Michael Ma <Michael.Ma@amerihome.com>
2019-07-08 10:50:49 -0700

    Trigger 1st build on Teamcity

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -7,6 +7,8 @@ namespace HelloWorldApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("This change will trigger a build on TeamCity!");
+            Console.Read();
         }
     }
 }

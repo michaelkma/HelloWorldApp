e8f91b11c54d2cfdcfef02c98bd9d90f88ff344d
Michael Ma <Michael.Ma@amerihome.com>
2019-07-08 10:58:33 -0700

    One

--- HelloWorldApp/Program.cs
+++ HelloWorldApp/Program.cs
@@ -6,13 +6,9 @@ namespace HelloWorldApp
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             Console.WriteLine("Hello World!");
             Console.WriteLine("This change will trigger a build on TeamCity!");
             Console.Read();
-=======
-            Console.WriteLine("Hello everyone!");
->>>>>>> 49004f2b64634fd3ef69e2a3d0d396945033bbfd
         }
     }
 }
